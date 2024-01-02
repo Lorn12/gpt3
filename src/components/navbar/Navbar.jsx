a14fa56cb3a930e12a2ee87356799127cb6784cd
Lauren <43707274+Lorn12@users.noreply.github.com>
2024-01-01 20:51:26 -0600

    Constructing navigation & implemented assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
+//FOR CLASS NAMES BEM METHOD IS USED -> BLOCK ELEMENT MODIFIER
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
