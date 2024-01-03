7df22dc372b097574263fdeb2c6e34707dcb6597
Lauren <43707274+Lorn12@users.noreply.github.com>
2024-01-03 15:44:15 -0600

    implemented article component styling

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -10,7 +10,7 @@ const Article = ({ imgUrl, date, title }) => {
       <div className="gpt3__blog-container_article-content">
         <div>
           <p>{date}</p>
-          <p>{title}</p>
+          <h3>{title}</h3>
         </div>
         <p>Read Full Article</p>
       </div>
