d9581069997399b3743ba9e8c4634e92eb024a25
Lauren <43707274+Lorn12@users.noreply.github.com>
2024-01-02 21:53:24 -0600

    Added styling

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -14,17 +14,12 @@ const Possibility = () => {
           The possibilities are beyond your imagination
         </h1>
         <p>
-          Yet bed any for travelling assistance indulgence unpleasing. Not
+          Yet bed any for traveling assistance indulgence unpleasing. Not
           thoughts all exercise blessing. Indulgence way everything joy
           alteration boisterous the attachment. Party we years to order allow
           asked of.
         </p>
-        <h4>
-          Yet bed any for travelling assistance indulgence unpleasing. Not
-          thoughts all exercise blessing. Indulgence way everything joy
-          alteration boisterous the attachment. Party we years to order allow
-          asked of.
-        </h4>
+        <h4>Request Early Access to Get Started</h4>
       </div>
     </div>
   );
